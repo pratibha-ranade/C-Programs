a8e13fdc0798c3a88a6027f1556e03107921a74c
Pratibha Ranade <90131135+pratibha-ranade@users.noreply.github.com>
2021-09-19 23:42:31 +0530

    Add files via upload

---
+++ Print_entered_number .c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+int num ;
+printf ( "Enter a number  " ) ;
+scanf ( "%d", &num ) ;
+printf ( "Now I am letting you on a secret...\n" ) ;
+printf ( "You have just entered the number %d\n", num ) ;
+return 0;
+}
